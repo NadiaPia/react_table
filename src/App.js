760e09a9954c1160617785fcf0d9a0ac174d115c
NadiaPia <206i417@gmail.com>
2023-01-30 17:45:03 -0800

    build the table

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { useTable } from 'react-table';
 
 function App() {
   const data = React.useMemo(() => fakeData, []); //according to the doc-n they recommend use usMemo
-  const columns = React.useMemo(() => [
+  const columns = React.useMemo(
+    () => [
     {
       Header: "ID",
       accessor: "id" //have to look like in the data
@@ -32,20 +33,41 @@ function App() {
       accessor: "university" 
     }
 
-  ]) //it returns array as columns are in a format of an array
+  ], []); //it returns array as columns are in a format of an array
   
   //const table = useTable({ columns, data }); //useTable({ columns, data }) returns a banch of ufnctions
+  
   const {getTableProps, getTableBodyProps, headerGroups, rows,  prepareRow } = useTable({ columns, data });
 
-
+// <table {...getTableProps()}> - method that returns the object of props that can be spread out into table elements 
+//<thead {...headerGroups.map((headerGroup) - the array of the objects that represents the header group of each table 
   return (
-    <div className="App">
+    <div className="App"> 
       <div className="container">
-        <table>
+        <table {...getTableProps()}>  
           <thead>
+             {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
 
           </thead>
-          <tbody>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                ))}
+              </tr>
+              )
+            })}
             
           </tbody>
         </table>
