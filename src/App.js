823651415ce8f79757f1cbac97dd8bcce32d629c
NadiaPia <206i417@gmail.com>
2023-01-30 16:24:35 -0800

    start building of the table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import fakeData from "./MOCK_DATA.json";
+import * as React from 'react';
+import { useTable } from 'react-table';
+
 
 function App() {
+  const data = React.useMemo(() => fakeData, []); //according to the doc-n they recommend use usMemo
+  const columns = React.useMemo(() => [
+    {
+      Header: "ID",
+      accessor: "id" //have to look like in the data
+    },
+    {
+      Header: "First name",
+      accessor: "first_name" 
+    },
+    {
+      Header: "Last name",
+      accessor: "last_name" 
+    },
+    {
+      Header: "Email",
+      accessor: "email" 
+    },
+    {
+      Header: "Gender",
+      accessor: "gender" 
+    },
+    {
+      Header: "University",
+      accessor: "university" 
+    }
+
+  ]) //it returns array as columns are in a format of an array
+  
+  //const table = useTable({ columns, data }); //useTable({ columns, data }) returns a banch of ufnctions
+  const {getTableProps, getTableBodyProps, headerGroups, rows,  prepareRow } = useTable({ columns, data });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <table>
+          <thead>
+
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
+      </div>
     </div>
+    
   );
 }
 
